cda84b564b05c9bb5e1a2751b8e41baf5aed1cee
Braian Medina <beybraian@gmail.com>
2023-07-17 18:37:43 -0300

    fix puntaje lvl2

--- Assets/Scripts/Puntaje.cs
+++ Assets/Scripts/Puntaje.cs
@@ -11,13 +11,20 @@ public class Puntaje : MonoBehaviour
 
     private void Start()
     {
-        puntos = GameObject.Find("ControladorPuntos").GetComponent<ControladorPuntos>().getPuntos();
+        if (GameObject.Find("ControladorPuntos")!=null)
+        {
+            puntos = GameObject.Find("ControladorPuntos").GetComponent<ControladorPuntos>().getPuntos();
+        }
+        
         textMesh = GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
-        textMesh.text = puntos.ToString("0");
+        if (textMesh != null) {
+            textMesh.text = puntos.ToString("0");
+        }
+        
         
             
     }
